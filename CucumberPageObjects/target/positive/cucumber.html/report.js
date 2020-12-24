d432e874fe603e61a04a980d8a89d6433e7413e3
Animesh Chandra <rikki.aqua@gmail.com>
2020-12-24 12:23:00 +0530

    second commit

--- CucumberPageObjects/target/positive/cucumber.html/report.js
+++ CucumberPageObjects/target/positive/cucumber.html/report.js
@@ -13,7 +13,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 6131551500,
+  "duration": 6216757000,
   "status": "passed"
 });
 formatter.scenario({
@@ -101,35 +101,35 @@ formatter.match({
   "location": "ValidateBankReportSteps.i_am_on_the_Homepage_of_Fitch_website(String)"
 });
 formatter.result({
-  "duration": 8785745300,
+  "duration": 8453479700,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_from_different_Page_Menus(String\u003e)"
 });
 formatter.result({
-  "duration": 549476000,
+  "duration": 561820900,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_Sectors_from_Insights_Menu()"
 });
 formatter.result({
-  "duration": 291603900,
+  "duration": 348948000,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_Banks_from_Financial_Institution()"
 });
 formatter.result({
-  "duration": 704725300,
+  "duration": 740424800,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_Insights_from_Banks_Page()"
 });
 formatter.result({
-  "duration": 3082547700,
+  "duration": 4239809200,
   "status": "passed"
 });
 formatter.match({
@@ -142,18 +142,18 @@ formatter.match({
   "location": "ValidateBankReportSteps.click_on_from_Insights(String)"
 });
 formatter.result({
-  "duration": 1500255500,
+  "duration": 161624300,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.print_the_link_that_is_Displaying()"
 });
 formatter.result({
-  "duration": 1484049300,
+  "duration": 1059990200,
   "status": "passed"
 });
 formatter.after({
-  "duration": 820832600,
+  "duration": 813132000,
   "status": "passed"
 });
 formatter.uri("ValidateEntityRating.feature");
@@ -171,7 +171,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 5463493000,
+  "duration": 5500511900,
   "status": "passed"
 });
 formatter.scenario({
@@ -228,14 +228,14 @@ formatter.match({
   "location": "ValidateBankReportSteps.i_am_on_the_Homepage_of_Fitch_website(String)"
 });
 formatter.result({
-  "duration": 7238977700,
+  "duration": 7815714700,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateEntityRatingSteps.click_on_Search_button()"
 });
 formatter.result({
-  "duration": 532780000,
+  "duration": 540551400,
   "status": "passed"
 });
 formatter.match({
@@ -248,32 +248,32 @@ formatter.match({
   "location": "ValidateEntityRatingSteps.enter_the(String)"
 });
 formatter.result({
-  "duration": 283945400,
+  "duration": 354052600,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateEntityRatingSteps.click_on_Enter()"
 });
 formatter.result({
-  "duration": 2454390300,
+  "duration": 3288128300,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateEntityRatingSteps.click_on_Entity()"
 });
 formatter.result({
-  "duration": 5855198300,
+  "duration": 1130978700,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateEntityRatingSteps.click_on_RAC_Reports()"
 });
 formatter.result({
-  "duration": 14849245100,
+  "duration": 6601794400,
   "status": "passed"
 });
 formatter.after({
-  "duration": 827896000,
+  "duration": 829028600,
   "status": "passed"
 });
 formatter.uri("ValidateFundandAssetManagerReport.feature");
@@ -291,7 +291,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 5384437300,
+  "duration": 5439160500,
   "status": "passed"
 });
 formatter.scenario({
@@ -379,36 +379,36 @@ formatter.match({
   "location": "ValidateBankReportSteps.i_am_on_the_Homepage_of_Fitch_website(String)"
 });
 formatter.result({
-  "duration": 7401378600,
+  "duration": 7515470200,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_from_different_Page_Menus(String\u003e)"
 });
 formatter.result({
-  "duration": 497978900,
+  "duration": 510655300,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateBankReportSteps.click_on_Sectors_from_Insights_Menu()"
 });
 formatter.result({
-  "duration": 283447400,
+  "duration": 317986300,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateFundandAsstMangerSteps.click_on_Funds_and_Asset_Manager_from_Financial_Institution()"
 });
 formatter.result({
-  "duration": 729123500,
+  "duration": 830815300,
   "status": "passed"
 });
 formatter.match({
   "location": "ValidateFundandAsstMangerSteps.click_on_Insights_from_Asset_Page()"
 });
 formatter.result({
-  "duration": 720987700,
-  "error_message": "org.openqa.selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document\n  (Session info: chrome\u003d87.0.4280.88)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/stale_element_reference.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027WKWIN5627505\u0027, ip: \u0027192.168.0.107\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002714.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 87.0.4280.88, chrome: {chromedriverVersion: 87.0.4280.88 (89e2380a3e36c..., userDataDir: C:\\Users\\anichand\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:57721}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 732a22b98b0d4bf5ef07d0e218603f0a\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:285)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.click(RemoteWebElement.java:84)\r\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:564)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat com.sun.proxy.$Proxy23.click(Unknown Source)\r\n\tat pages.action.FundsandAssetPageAction.clickmoreInsightsfunds(FundsandAssetPageAction.java:22)\r\n\tat Steps.ValidateFundandAsstMangerSteps.click_on_Insights_from_Asset_Page(ValidateFundandAsstMangerSteps.java:20)\r\n\tat ✽.And Click on Insights from Asset Page(ValidateFundandAssetManagerReport.feature:14)\r\n",
+  "duration": 739418800,
+  "error_message": "org.openqa.selenium.StaleElementReferenceException: stale element reference: element is not attached to the page document\n  (Session info: chrome\u003d87.0.4280.88)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/stale_element_reference.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027WKWIN5627505\u0027, ip: \u0027192.168.0.107\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_202\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 87.0.4280.88, chrome: {chromedriverVersion: 87.0.4280.88 (89e2380a3e36c..., userDataDir: C:\\Users\\anichand\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:62505}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 567577a8903e02d7bd7c4e632c28b56d\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:285)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.click(RemoteWebElement.java:84)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat com.sun.proxy.$Proxy18.click(Unknown Source)\r\n\tat pages.action.FundsandAssetPageAction.clickmoreInsightsfunds(FundsandAssetPageAction.java:22)\r\n\tat Steps.ValidateFundandAsstMangerSteps.click_on_Insights_from_Asset_Page(ValidateFundandAsstMangerSteps.java:20)\r\n\tat ✽.And Click on Insights from Asset Page(ValidateFundandAssetManagerReport.feature:14)\r\n",
   "status": "failed"
 });
 formatter.match({
@@ -431,7 +431,7 @@ formatter.result({
 });
 formatter.embedding("image/png", "embedded0.png");
 formatter.after({
-  "duration": 2223586800,
+  "duration": 3008505400,
   "status": "passed"
 });
 });
